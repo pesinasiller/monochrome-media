31008c2f192e5f41364040867b321b7276362fb6
José Carlos Pesina Siller <pesinasiller@MacBook-Pro-de-Jose-2.local>
2021-01-11 13:26:30 -0600

    Updates

---
+++ precache-manifest.9b37ded8c162484c2adfc2f19d5c701b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa709c4ad582e55ad0a1ab5ed1f805d",
+    "url": "/monochrome-media/index.html"
+  },
+  {
+    "revision": "8caa2eb0230ddc20dbeb",
+    "url": "/monochrome-media/static/css/main.04b4476b.chunk.css"
+  },
+  {
+    "revision": "ae21db4c755f0e9d8c8d",
+    "url": "/monochrome-media/static/js/2.8238beb3.chunk.js"
+  },
+  {
+    "revision": "9c08907785ba52840034efcaae400ffd",
+    "url": "/monochrome-media/static/js/2.8238beb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8caa2eb0230ddc20dbeb",
+    "url": "/monochrome-media/static/js/main.727593aa.chunk.js"
+  },
+  {
+    "revision": "e980eb9237f79be14207",
+    "url": "/monochrome-media/static/js/runtime-main.4a5e13d7.js"
+  },
+  {
+    "revision": "8a13f42547ffdf7ebc7fc9b6d68a0c86",
+    "url": "/monochrome-media/static/media/AccidentalPresidency.8a13f425.ttf"
+  },
+  {
+    "revision": "a0ec7951b44ceed54e209a036b725bec",
+    "url": "/monochrome-media/static/media/AccidentalPresidency.a0ec7951.woff"
+  },
+  {
+    "revision": "af659c1c22746b0694f0c461998871da",
+    "url": "/monochrome-media/static/media/AccidentalPresidency.af659c1c.woff2"
+  },
+  {
+    "revision": "b514f1dcc5b5c6123bb6d83a96edc41e",
+    "url": "/monochrome-media/static/media/AccidentalPresidency.b514f1dc.eot"
+  },
+  {
+    "revision": "1b7faf4a89acfcb149edca4868b9619b",
+    "url": "/monochrome-media/static/media/anson-regular-webfont.1b7faf4a.woff"
+  },
+  {
+    "revision": "284cb465b6c6f0996ca0b16bc5f394ea",
+    "url": "/monochrome-media/static/media/anson-regular-webfont.284cb465.ttf"
+  },
+  {
+    "revision": "bec5bfc09f21498afb8d2e60ea11fbdf",
+    "url": "/monochrome-media/static/media/anson-regular-webfont.bec5bfc0.eot"
+  },
+  {
+    "revision": "ce985ce31ce5257f841531eafadf3186",
+    "url": "/monochrome-media/static/media/anson-regular-webfont.ce985ce3.svg"
+  },
+  {
+    "revision": "756d5578e6b8096de041946f0d74fcbf",
+    "url": "/monochrome-media/static/media/logo-big.756d5578.svg"
+  },
+  {
+    "revision": "a3aa179653046a8c4ac092aaf8714525",
+    "url": "/monochrome-media/static/media/logo-small.a3aa1796.svg"
+  }
+]);
